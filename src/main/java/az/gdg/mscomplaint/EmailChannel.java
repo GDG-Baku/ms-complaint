685e41514013e62ebd2c74ef0ac4fa92128ea2fa
Ali Huseynov <47367245+alihsoff@users.noreply.github.com>
2020-03-31 17:36:43 +0400

    Delete EmailChannel.java

--- src/main/java/az/gdg/mscomplaint/EmailChannel.java
+++
@@ -1,9 +0,0 @@
-package az.gdg.mscomplaint;
-
-import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.messaging.MessageChannel;
-
-public interface EmailChannel {
-    @Output("emailOutput")
-    MessageChannel emailOutput();
-}
