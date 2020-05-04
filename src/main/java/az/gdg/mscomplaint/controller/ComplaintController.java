85384caba31e2c86daa1824d3c1ac0c5e13d1457
alihsoff <alihsoff@gmail.com>
2020-05-04 21:37:32 +0400

    BE-21: fix import issue

--- src/main/java/az/gdg/mscomplaint/controller/ComplaintController.java
+++ src/main/java/az/gdg/mscomplaint/controller/ComplaintController.java
@@ -6,6 +6,7 @@ import az.gdg.mscomplaint.service.ComplaintService;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
